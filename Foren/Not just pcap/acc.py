e318b936c42553ca1507db0ad5587439bd34790e
Automic_Kaiii <94669750+KMANVK@users.noreply.github.com>
2023-09-13 20:56:06 +0700

    Update acc.py

--- Foren/Not just pcap/acc.py
+++ Foren/Not just pcap/acc.py
@@ -1,25 +1,25 @@
-import sys
+def add_adts(raw_packet):
+    profile = 2#  //AAC LC
+    freqIdx = 4#  //44.1KHz
+    chanCfg = 2#  //CPE
+    packetLen = len(raw_packet) + 7
 
-import tqdm
-import scapy.utils
+    # fill in ADTS data
+    packet = bytearray(7)
+    packet[0] = 0xFF
+    packet[1] = 0xF9
+    packet[2] = (((profile-1)<<6) + (freqIdx<<2) +(chanCfg>>2))
+    packet[3] = (((chanCfg&3)<<6) + (packetLen>>11))
+    packet[4] = ((packetLen&0x7FF) >> 3)
+    packet[5] = (((packetLen&7)<<5) + 0x1F)
+    packet[6] = 0xFC
+    return packet + raw_packet
 
-LOAS_SYNC = 0x2b7
-LATM_HEADER = b"\x47\xfc\x00\x00"
+f = open('out.aac', 'wb')
 
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: loas.py <pcap> <loas>")
-        exit(1)
-
-    pkts = scapy.utils.rdpcap(sys.argv[1])
-
-    with open(sys.argv[2], "wb") as f:
-        for i, pkt in tqdm.tqdm(enumerate(pkts), total=len(pkts)):
-            data = bytes(pkt)
-            payload = data[data.index(LATM_HEADER):]
-
-            # append loas_header for each aac-latm frame
-            loas_header = (LOAS_SYNC << 13) + (len(payload) & 0x1fff)
-            f.write(loas_header.to_bytes(3, 'big'))
-            f.write(payload)
+i = 0
+for line in open('raw_hex', 'r'):
+    line_bytes = bytes.fromhex(line)
+    packet = line_bytes[12:]
+    packet = add_adts(packet)
+    f.write(packet)
